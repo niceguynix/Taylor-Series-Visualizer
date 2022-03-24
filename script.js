120f7ff7dbf6e05963bcf4b038eecc0aa9480418
manoj <its200manoj@gmail.com>
2022-03-24 06:29:13 +0530

    added term selector

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let input = document.getElementById("func_input");
 let calc = document.getElementById("calc");
 let series = document.getElementById("series");
+let terms = document.getElementById("no_of_terms");
 
 calc.addEventListener("click", () => {
+  console.log(typeof terms.value)
   //   console.log(input.value);
   //   console.log(math.derivative(input.value, "x").toString());
   //series.innerText = math.derivative(input.value, 'x').toString();
-  taylor = get_taylor_terms(input.value, 20);
+  taylor = get_taylor_terms(input.value, terms.value ? terms.value : 5  );
   katex.render(taylor, series, {
     throwOnError: false,
   });
