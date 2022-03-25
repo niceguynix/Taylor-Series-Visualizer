42b2770c03a5029126c96c6a6c2b27630c5f30c8
manoj <its200manoj@gmail.com>
2022-03-25 06:54:32 +0530

    .

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 let input = document.getElementById("func_input");
 let calc = document.getElementById("calc");
-let series = document.getElementById("series");
+let series = document.getElementById("taylor");
 let terms = document.getElementById("no_of_terms");
+let start = document.getElementById("s");
 
 calc.addEventListener("click", () => {
-  console.log(typeof terms.value)
+  katex.render(input.value, original, {
+    throwOnError: false,
+  }); 
   //   console.log(input.value);
   //   console.log(math.derivative(input.value, "x").toString());
   //series.innerText = math.derivative(input.value, 'x').toString();
-  taylor = get_taylor_terms(input.value, terms.value ? terms.value : 5  );
-  katex.render(taylor, series, {
+  [taylor,round] = get_taylor_terms(input.value, terms.value ? terms.value : 5 ,s.value ? s.value : 0);
+  katex.render(round, series, {
     throwOnError: false,
   });
+  a = math.simplify(taylor).toString();
   functionPlot({
-    target: "#graph",
+    target: "#taylor-graph",
+    title: "Taylor Series",
+    with:400,
+    height:400,
     data: [
       {
         fn: input.value,
@@ -22,7 +29,26 @@ calc.addEventListener("click", () => {
       { fn: taylor, 
         // updateOnMouseMove: true, 
         graphType: "polyline"
-       },
+       }, 
+      //  { fn: `0.3010+(0.5/1!* x - 2)+(-0.25/2!* (x - 2) ^ 2)+(0.25/3!* (x - 2) ^ 3)+(-0.375/4!* (x - 2) ^ 4)`,
+      //   // updateOnMouseMove: true, 
+      //   graphType: "polyline"
+      //  },
     ],
   });
+
+
+  functionPlot({
+    target: "#error",
+    title: "Absolute Error",
+    with:400,
+    height:400,
+    data: [
+      { fn: `abs(${taylor} - ${input.value})`, 
+        graphType: "polyline"
+       }, 
+    ],
+  });
+
+
 });
